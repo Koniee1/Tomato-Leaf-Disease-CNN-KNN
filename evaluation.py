26d8ade710cafa02c9703c08f9de480ab4371cc5
Bishio Ruth Koni <152609000+Koniee1@users.noreply.github.com>
2026-01-15 22:00:25 +0100

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,19 @@
+# Line graphs
+plt.figure(figsize=(12,5))
+plt.subplot(1,2,1)
+plt.plot(history.history['accuracy'], label='Train Acc')
+plt.plot(history.history['val_accuracy'], label='Val Acc')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(); plt.title('Line Graph of Accuracy')
+plt.subplot(1,2,2)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(); plt.title('Line graph of Loss')
+plt.show()
+
 # Confusion matrix
 cm = confusion_matrix(y_val, y_pred)
 plt.figure(figsize=(7,5))
